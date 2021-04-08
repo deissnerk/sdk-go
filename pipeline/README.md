9548706d34a7102d9b8dce1307c63da34c29a7d5
Klaus Deissner <klaus.deissner@sap.com>
2021-04-08 12:04:46 +0200

    Worked on router
    Signed-off-by: Klaus Deissner <klaus.deissner@sap.com>

--- pipeline/README.md
+++ pipeline/README.md
@@ -1,26 +1,31 @@
 # Event Pipe
 
 ## Intro
+
 _Event Pipe_ defines several components that can be combined in a flexible way
-to construct pipelines for processing CloudEvents. An example of such a
-pipeline could be an inbound component fetching CloudEvents from an AMQP
-link and passing them on to one or multiple targets enriching and validating
-the events on the way. All steps are asynchronous, and a configurable
-sliding window provides the means to absorb short bursts.
+to construct pipelines for processing CloudEvents. An example of such a pipeline
+could be an inbound component fetching CloudEvents from an AMQP link and passing
+them on to one or multiple targets enriching and validating the events on the
+way. All steps are asynchronous, and a configurable sliding window provides the
+means to absorb short bursts.
 
 ## Pipelines
-Pipelines consist of a sequence of processors that process tasks. Each task
-is based on a CloudEvent. Processors can return results, trigger external
-logic and pass additional information on to the next processor. 
+
+Pipelines consist of a sequence of processors that process tasks. Each task is
+based on a CloudEvent. Processors can return results, trigger external logic and
+pass additional information on to the next processor.
 
 ### Tasks
-A task encapsulates the information that is passed between event processors
-. It contains the event that is processed, a context object, and a list of
- changes preceding processors have already added. For the event the original
-  `binding.Message` is used to allow highly optimized flows.   
+
+A task encapsulates the information that is passed between event processors . It
+contains the event that is processed, a context object, and a list of changes
+preceding processors have already added. For the event the original
+`binding.Message` is used to allow highly optimized flows.
 
 ### Processors
+
 A processor takes a `Task` as input and returns a `ProcessorOutput`:
+
 ```
 type ProcessorOutput struct {
 	Result   TaskResult
@@ -28,11 +33,13 @@ type ProcessorOutput struct {
 	Changes  []binding.Transformer
 }
 ```
+
 This output can contain a result or error, a context and a list of event
-transformers. While the result is passed back to the initiator of the
-pipeline, the context is used to call the next processor. 
+transformers. While the result is passed back to the initiator of the pipeline,
+the context is used to call the next processor.
 
 ## Combining Pipelines
+
 Pipelines can be combined using different elements.
 
 ### Inbound
@@ -42,11 +49,10 @@ event messages from outside and feed them into the pipeline.
 
 ### Split/Join
 
-The split/join element can be used to split a task into sub tasks that are
-processed in parallel. Of course each sub task can again be processed by a
-dedicated pipeline.
-After the parallel execution the results can be joined and the next step of
-the main pipeline can be triggered. 
+The split/join element can be used to split a task into sub-tasks that are
+processed in parallel. Of course each sub-task can again be processed by a
+dedicated pipeline. After the parallel execution the results can be joined, and
+the next step of the main pipeline can be triggered.
 
 ### Router
 
@@ -57,34 +63,27 @@ pipeline.
 
 To provide the abstraction of pipelines and processors, a runtime model of
 runners and supervisors is used. Runners have a `Push()` method that takes a
-`TaskContainer` as input:
-```
-type TaskContainer struct {
-	Callback chan *StatusMessage
-	Key      interface{}
-	Task     Task
-	Parent   *TaskContainer
-}
-```
-The `TaskContainer` contains all information a `Runner` needs to process a
-task. There are two implementations of the `Runner` interface.
+`TaskContainer` as input.
+
+The `TaskContainer` contains all information a `Runner` needs to process a task.
+There are two implementations of the `Runner` interface.
 
 ### Workers
 
-A worker invokes a processor, passes the result to a callback channel and
-pushes the task to the next worker in the pipeline runtime.
+A worker invokes a processor, passes the result to a callback channel and pushes
+the task to the next worker in the pipeline runtime.
 
 ### Supervisors
 
 Supervisors are used as base of several pipeline elements like `Inbound
-`, `Splitter` or `Router`. Supervisors constitute the beginning of a pipeline
-. They maintain the state of the tasks that are currently travelling through
-the pipeline. The existing implementations of `SupervisorState` apply a
-sliding window algorithm.  
- 
+`, `Splitter` or `Router`. Supervisors constitute the beginning of a pipeline .
+They maintain the state of the tasks that are currently travelling through the
+pipeline. The existing implementations of `SupervisorState` apply a sliding
+window algorithm.
+
 ## Examples
-  
-## Future 
+
+## Future
 
 ### Describe Pipelines in mark-up
 
--- pipeline/elements/router.go
+++ pipeline/elements/router.go
@@ -1,178 +1,148 @@
 package elements
 
-//import (
-//	"context"
-//	"github.com/cloudevents/sdk-go/pipeline"
-//	"github.com/cloudevents/sdk-go/v2/protocol"
-//	"github.com/pkg/errors"
-//)
-//
-///*type splitterTaskKey struct {
-//	key    pipeline.TaskIndex
-//	subKey pipeline.TaskIndex
+import (
+	"context"
+	"github.com/cloudevents/sdk-go/pipeline"
+	"github.com/cloudevents/sdk-go/v2/protocol"
+	"github.com/pkg/errors"
+)
+
+/*type splitterTaskKey struct {
+	key    pipeline.TaskIndex
+	subKey pipeline.TaskIndex
+}
+
+type splitterTaskStatus struct {
+	j Joiner
+	tc []*pipeline.TaskControl
+}*/
+
+type Router interface {
+	Route(origin *pipeline.Task) *TaskAssignment
+
+	pipeline.StartStop
+}
+
+type RouterConstructor func() (Router, error)
+
+type RouterRunner struct {
+	state *RouterState
+	sv    *pipeline.Supervisor
+}
+
+func (s *RouterRunner) Push(tc *pipeline.TaskContainer) {
+	s.sv.Push(tc)
+}
+
+func NewRouterRunner(r Router, id pipeline.ElementId, nextStep pipeline.Runner) (*RouterRunner, error) {
+	state := &RouterState{
+		id: id,
+
+		sw:       pipeline.NewSlidingWindow(pipeline.DefaultWS),
+		nextStep: nextStep,
+	}
+
+	return &RouterRunner{
+		state: state,
+		sv:    pipeline.NewSupervisor(state),
+	}, nil
+}
+
+func (s RouterRunner) Start() error {
+	// The Supervisor will start the state and all related resources
+	return s.sv.Start()
+}
+
+func (s RouterRunner) Stop(ctx context.Context) {
+	s.sv.Stop(ctx)
+}
+
+func (s RouterRunner) Id() pipeline.ElementId {
+	return s.state.id
+}
+
+var _ pipeline.Runner = (*RouterRunner)(nil)
+
+//func (s SplitterRunner) SetNextStep(runner pipeline.Runner) {
+//	s.state.SetNextStep(runner)
 //}
-//
-//type splitterTaskStatus struct {
-//	j Joiner
-//	tc []*pipeline.TaskControl
-//}*/
-//
-//type Router interface {
-//	Route(origin *pipeline.Task) ([]*TaskAssignment,Joiner)
-//
-//	pipeline.StartStop
-//}
-//
-//type RouterConstructor func() (Router,error)
-//
-//type RouterRunner struct {
-//	state *SplitterState
-//	sv    *pipeline.Supervisor
-//}
-//
-//func (s *RouterRunner) Push(tc *pipeline.TaskContainer) {
-//	s.sv.Push(tc)
-//}
-//
-//func NewRouterRunner(r Router, id pipeline.ElementId, nextStep pipeline.Runner) (*SplitterRunner,error) {
-//	state := &RouterState{
-//		id:       id,
-//
-//		sw:       pipeline.NewSlidingWindow(pipeline.DefaultWS),
-//		nextStep: nextStep,
-//	}
-//
-//	return &SplitterRunner{
-//		state: state,
-//		sv:pipeline.NewSupervisor(state),
-//	},nil
-//}
-//
-//func (s RouterRunner) Start() error{
-//	// The Supervisor will start the state and all related resources
-//	return s.sv.Start()
-//}
-//
-//func (s RouterRunner) Stop(ctx context.Context) {
-//	s.sv.Stop(ctx)
-//}
-//
-//func (s RouterRunner) Id() pipeline.ElementId {
-//	return s.state.id
-//}
-//
-//var _ pipeline.Runner = (*RouterRunner)(nil)
-//
-////func (s SplitterRunner) SetNextStep(runner pipeline.Runner) {
-////	s.state.SetNextStep(runner)
-////}
-//
-//// The default SupervisorState implementation. It maintains a window and sets finishes a task, when all sub-tasks
-//// are finished.
-//type RouterState struct {
-//	id pipeline.ElementId
-//	ts Router
-//	//	maxWSize pipeline.TaskIndex
-//	//	wsc      WindowStateCallback
-//	sw       *pipeline.SlidingWindow
-//	nextStep pipeline.Runner
-//}
-//
-//func (st *RouterState) Id() pipeline.ElementId {
-//	return st.id
-//}
-//
-//func (st *RouterState) Start() error {
-//	return st.ts.Start()
-//}
-//
-//// Stop() calls Stop() on the Splitter and for the next step to propagate the Stop()
-//// call through the pipeline. The state is only stopped after the Supervisor was stopped.
-//func (st *RouterState) Stop(ctx context.Context) {
-//	st.ts.Stop(ctx)
-//	//	st.nextStep.Stop()
-//}
-//
-////func (st *SplitterState) SetNextStep(step pipeline.Runner) {
-////	st.nextStep = step
-////}
-//
-//func (st *RouterState) AddTask(tc *pipeline.TaskContainer, callback chan *pipeline.StatusMessage) {
-//	tas,j := st.ts.Split(&tc.Task)
-//
-//	if len(tas) == 0 {
-//		tc.SendStatusUpdate(st.id, pipeline.TaskResult{
-//			Error:  errors.Errorf("Splitter returned no tasks"),
-//			Result: nil,
-//		}, true)
-//		return
-//	}
-//	svt, key := st.sw.AddTask()
-//
-//	stc := make([]*pipeline.TaskControl, len(tas))
-//
-//	for i, ta := range tas {
-//		id := ta.Runner.Id()
-//		stc[i] = &pipeline.TaskControl{
-//			Status: pipeline.TaskStatus{
-//				Id: id,
-//				//Key: splitterTaskKey{
-//				//	key:    key,
-//				//	subKey: pipeline.TaskIndex(i),
-//				//},
-//				Finished: false,
-//			},
-//			Cancel: ta.Cancel,
-//		}
-//	}
-//
-//	*svt = pipeline.SuperVisorTask{
-//		Main:  tc,
-//		TStat: &splitterTaskStatus{
-//			j:  j,
-//			tc: stc,
-//		},
-//	}
-//
-//	for i, ta := range tas {
-//		ta.Runner.Push(
-//			&pipeline.TaskContainer{
-//				Callback: callback,
-//				Key: splitterTaskKey{
-//					key:    key,
-//					subKey: pipeline.TaskIndex(i),
-//				},
-//				Task:    *ta.Task,
-//				Parent:  tc,
-//			})
-//	}
-//}
-//
-//func (st *RouterState) IsIdle() bool {
-//	return st.sw.IsEmpty()
-//}
-//
-//// Returns true, if the window is empty
-//func (st *RouterState) UpdateTask(sMsg *pipeline.StatusMessage) bool {
-//	tKey := sMsg.Key.(splitterTaskKey)
-//	svt := st.sw.GetSupervisorTask(tKey.key)
-//	tStat := svt.TStat.(*splitterTaskStatus)
-//	tStat.tc[tKey.subKey].Status = sMsg.Status
-//
-//	if tStat.j.IsFinished(tStat.tc) {
-//		joinStat := tStat.j.Join(tStat.tc)
-//		if st.nextStep != nil && protocol.IsACK(joinStat.Result.Error) {
-//			// If there is a next step, push the main task to it and send the result as a status update
-//			svt.Main.SendStatusUpdate(st.id, joinStat.Result, false)
-//			svt.Main.AddOutput(joinStat)
-//			st.nextStep.Push(svt.Main)
-//		} else {
-//			svt.Main.SendStatusUpdate(st.id, joinStat.Result, true)
-//		}
-//		return st.sw.RemoveTask(tKey.key)
-//	}
-//
-//	return false
-//}
-//
-//var _ pipeline.SupervisorState = (*SplitterState)(nil)
+
+// The default SupervisorState implementation. It maintains a window and sets finishes a task, when all sub-tasks
+// are finished.
+type RouterState struct {
+	id pipeline.ElementId
+	ts Router
+	//	maxWSize pipeline.TaskIndex
+	//	wsc      WindowStateCallback
+	sw       *pipeline.SlidingWindow
+	nextStep pipeline.Runner
+}
+
+func (st *RouterState) Id() pipeline.ElementId {
+	return st.id
+}
+
+func (st *RouterState) Start() error {
+	return st.ts.Start()
+}
+
+// Stop() calls Stop() on the Splitter and for the next step to propagate the Stop()
+// call through the pipeline. The state is only stopped after the Supervisor was stopped.
+func (st *RouterState) Stop(ctx context.Context) {
+	st.ts.Stop(ctx)
+	//	st.nextStep.Stop()
+}
+
+func (st *RouterState) AddTask(tc *pipeline.TaskContainer, callback chan *pipeline.StatusMessage) {
+	ta := st.ts.Route(tc.Task)
+
+	if ta == nil {
+		tc.SendStatusUpdate(st.id, pipeline.TaskResult{
+			Error:  errors.Errorf("Router returned no route"),
+			Result: nil,
+		}, true)
+		return
+	}
+	svt, key := st.sw.AddTask()
+
+	*svt = pipeline.SuperVisorTask{
+		Main: tc,
+		TStat: &pipeline.TaskStatus{
+			Finished: false,
+		},
+	}
+
+	ta.Runner.Push(tc.NewChild(callback,key,ta.Task))
+}
+
+func (st *RouterState) IsIdle() bool {
+	return st.sw.IsEmpty()
+}
+
+// Returns true, if the window is empty
+func (st *RouterState) UpdateTask(sMsg *pipeline.StatusMessage) bool {
+	tKey := sMsg.Key.(pipeline.TaskIndex)
+	svt := st.sw.GetSupervisorTask(tKey)
+	tStat := svt.TStat.(*pipeline.TaskStatus)
+	tStat = &sMsg.Status
+
+	if tStat.Finished {
+		if st.nextStep != nil && protocol.IsACK(tStat.Result.Error) {
+			// If there is a next step, push the main task to it and send the result as a status update
+			svt.Main.SendStatusUpdate(st.id, tStat.Result, false)
+			svt.Main.AddOutput(&pipeline.ProcessorOutput{
+				Result:   sMsg.Status.Result,
+				FollowUp: nil,
+				Changes:  nil,
+			})
+			st.nextStep.Push(svt.Main)
+		} else {
+			svt.Main.SendStatusUpdate(st.id, joinStat.Result, true)
+		}
+		return st.sw.RemoveTask(tKey.key)
+	}
+
+	return false
+}
+
+var _ pipeline.SupervisorState = (*SplitterState)(nil)
